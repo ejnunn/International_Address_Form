db8502a3db07f176a18b6aa26c2c7542a2ddfe71
Jacob Miller <millerjacob@seattleu.edu>
2020-02-21 12:38:29 -0800

    Added Controller/Models

---
+++ Models/Form.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Form
+{
+    public string AddressLine0 { get; set; }
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set; }
+    public string Country { get; set; }
+    public string Region { get; set; }
+    public string locale { get; set; }
+    public int Zip { get; set; }
+}
