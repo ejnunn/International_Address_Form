2be3861b7591432e53712b8768b74daaab7ab269
jbmil <jbmiller015@gmail.com>
2020-02-23 15:51:39 -0800

    Deleted bad file

--- Models/Form.cs
+++ Models/Form.cs
@@ -17,7 +17,7 @@ namespace International_Address_Form.Models
 
     public class Country
     {
-        public string Country { get; set; }
+        public string country { get; set; }
         public string RegionType { get; set; }
         public string localeType { get; set; }
         public int CodeLenght { get; set; }
